73bf9c1bc80f45b5c59dbf13f77d15de0ce4b8ee
s0k0bhp <soham.kundu@walmart.com>
2024-09-13 22:30:23 +0530

    project created

---
+++ src/test/java/com/soham/ems_backend/EmsBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.soham.ems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+
+
+}
